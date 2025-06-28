e1f9831706e64f7e35f4bba05033e58ec1f2b0cb
Rifleborn <butcherimpg@gmail.com>
2025-06-28 18:18:19 +0300

    basic UI and loginToClient function

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 import kivy
 kivy.require('2.0.0')
 # kivymd - 1.1.1
-from gdataprocessor import loadGoogleData
 
 import logging
-
-import asyncio
-from threading import Thread
-
-# This modifies the console output
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -21,12 +15,18 @@ from kivy.uix.scrollview import ScrollView
 from kivy.uix.button import Button
 from kivymd.app import MDApp
 
-# data table
 from kivy.metrics import dp
 from kivymd.uix.datatables import MDDataTable
 from kivymd.uix.button import MDRaisedButton
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.spinner import MDSpinner
+
+import asyncio
+from threading import Thread
+
+import json
+
+from telethon import TelegramClient
 
 loop = asyncio.new_event_loop()
 
@@ -36,20 +36,6 @@ class MainWidget(Widget):
 
 # loads .kv file with monitor### name
 class Monitor(MDApp):
-    # Start asyncio event loop in background thread
-
-    # second is optional button instance
-    def loadMoreRecords(self, instance=None):
-        if (self.loadMoreIsPressed == False):
-            self.loadMoreIsPressed = True
-
-            self.tableSpinner.opacity = 1
-            self.tableSpinner.disabled = False
-
-            asyncio.run_coroutine_threadsafe(loadGoogleData(self, 5), self.async_loop)
-
-        else:
-            print("[ARDUINO PROJECT] Error: button loadMore already pressed! Loading skipped.")
 
     def build(self):
 
@@ -61,7 +47,7 @@ class Monitor(MDApp):
         self.rootWidget = MainWidget()
 
         self.async_loop = asyncio.new_event_loop()
-        Thread(target=self.start_loop, daemon=True).start()
+        Thread(target=self.startLoop, daemon=True).start()
 
         self.mainTable = MDDataTable(
             use_pagination=True,
@@ -70,10 +56,8 @@ class Monitor(MDApp):
             column_data=[
                 ("Час", dp(15)),
                 ("Дата", dp(19)),
-                ("Teмп.", dp(12)),
-                ("Вологість", dp(17)),
-                ("Повітря", dp(16)),
-                ("Дощ", dp(21)),
+                ("Текст", dp(12)),
+                ("Номер", dp(17)),
             ],
             row_data=[],
             elevation=2,
@@ -89,27 +73,88 @@ class Monitor(MDApp):
             active=True
         )
 
-        self.rootWidget.ids.table_container.add_widget(self.tableSpinner)
-
         button = MDRaisedButton(
-            text="Завантажити ще",
+            text="Завантажити",
             # size_hint=(1, 0.15),
             size_hint=(0.8, 0.1),
             pos_hint = {"center_x": 0.5},
             font_style="H5",  # Більший і жирний текст
             md_bg_color=(0.224, 0.800, 0.776, 1),
+            on_release=self.loadButtonAction,
         )
 
-        button.bind(on_release=self.loadMoreRecords)
         self.rootWidget.ids.footer.add_widget(button)
 
-        self.loadMoreRecords()
+        with open("credentials/creds.json") as f:
+            config = json.load(f)
+
+        self.rootWidget.ids.idField.text = str(config['api_id'])
+        self.rootWidget.ids.hashField.text = str(config['api_hash'])
+        self.rootWidget.ids.phoneNumberField.text = str(config['phone_number'])
 
         return self.rootWidget
 
-    def start_loop(self):
+    def loadButtonAction(self, instance):
+        asyncio.run_coroutine_threadsafe(self.loginToClient(), self.async_loop)
+
+    async def loginToClient(self):
+        print('[DEBUG] --Connecting to telegram--')
+
+        try:
+            client = TelegramClient('default_session', self.rootWidget.ids.idField.text, self.rootWidget.ids.hashField.text)
+            phoneNumber = self.rootWidget.ids.phoneNumberField.text
+
+            await client.connect()
+
+            # check if client not authorized
+            if not await client.is_user_authorized():
+
+                # auth code
+                try:
+                    await client.send_code_request(phoneNumber)
+                except Exception as e:
+                    print(f"[ERROR] Unexpected error: {e}")
+                    return
+
+                verificationCode = input("Enter the code you received:")
+
+                # 2fa
+                try:
+                    await client.sign_in(phoneNumber, verificationCode)
+                except Exception as e:
+                    print(f"[ERROR] Unexpected error: {e}")
+
+                    twoFactorCode = input("[DEBUG] 2FA:")
+                    await client.sign_in(password=twoFactorCode)
+
+            # testing connection
+            print(await client.get_me().stringify())
+            await client.send_message('me', 'Hello to myself!')
+
+        except Exception as e:
+            print(f"[ERROR] Unexpected error: {e}")
+
+        finally:
+            if 'client' in locals():
+                client.disconnect()
+
+    def startLoop(self):
         asyncio.set_event_loop(self.async_loop)
         self.async_loop.run_forever()
+
+    def showError(self, message):
+        if not self.dialog:
+            self.dialog = MDDialog(
+                title="Error",
+                text=message,
+                buttons=[
+                    MDFlatButton(
+                        text="OK", on_release=lambda x: self.dialog.dismiss()
+                    ),
+                ],
+            )
+        self.dialog.text = message
+        self.dialog.open()
 
 # run application
 if __name__ == '__main__':
